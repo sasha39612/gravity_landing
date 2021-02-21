c09c7798d64f4d3cab0800e7a5d05f4978398a5a
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-02-21 09:50:05 +0200

    Landing first project

--- dist/Landing.e31bb0bc.js
+++ dist/Landing.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59158" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49929" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -324,4 +324,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/Landing.e31bb0bc.js.map
+//# sourceMappingURL=/landing.e31bb0bc.js.map
